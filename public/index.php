0417fdb1ed6ab5e0bbbea8670e789e245fb91d30
Alex Vogel <avogelm@gmail.com>
2018-04-05 11:13:38 +0200

    Añadido a dependencias el conector de slim con twig. Fichero index.php inicializado y terminado (dos lineas kappa)

--- public/index.php
+++ public/index.php
@@ -5,3 +5,19 @@
  * Date: 04/04/2018
  * Time: 20:56
  */
+
+$app = new \Slim\App;
+require __DIR__.'/../app/routes.php';
+
+try {
+    $app->run();
+} catch (\Slim\Exception\MethodNotAllowedException $e) {
+    echo $e->getMessage();
+    http_response_code(500);
+} catch (\Slim\Exception\NotFoundException $e) {
+    echo $e->getMessage();
+    http_response_code(500);
+} catch (Exception $e) {
+    echo $e->getMessage();
+    http_response_code(500);
+}
