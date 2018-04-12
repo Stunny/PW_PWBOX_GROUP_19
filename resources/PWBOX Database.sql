1eb97a9162f3d26737603f805315081668a989d4
Alex Vogel <avogelm@gmail.com>
2018-04-12 12:21:01 +0200

    Actualizado el script de creacion de tablas con los campos created_at y updated_at. Modificadas propiedades del resto de campos

--- resources/PWBOX Database.sql
+++ resources/PWBOX Database.sql
@@ -6,30 +6,32 @@ DROP TABLE IF EXISTS Folder CASCADE;
 DROP TABLE IF EXISTS Usuari CASCADE;
 
 CREATE TABLE Usuari (
-	id INT NOT NULL AUTO_INCREMENT,
-    nom VARCHAR(255),
-    contrasenya VARCHAR(255),
-    email VARCHAR(255),
-    birthdate DATE,
-    profile_image VARCHAR(255),
-    verified BOOLEAN DEFAULT false,
+	`id` INT NOT NULL AUTO_INCREMENT,
+    `nom` VARCHAR(255) NOT NULL,
+    `contrasenya` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(255) NOT NULL,
+    `profile_image` VARCHAR(255),
+    `verified` BOOLEAN DEFAULT false,
+	`created_at` DATETIME NOT NULL,
+	`updated_at` DATETIME NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE Folder(
-	id INT NOT NULL AUTO_INCREMENT,
-    creador INT,
-    nom VARCHAR(255),
-    path VARCHAR(255),
+	`id` INT NOT NULL AUTO_INCREMENT,
+    `creador` INT NOT NULL,
+    `nom` VARCHAR(255) NOT NULL,
+    `path` VARCHAR(255) NOT NULL,
+	`created_at` DATETIME NOT NULL,
+	`updated_at` DATETIME NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (creador) REFERENCES Usuari(id)
 );
 
 CREATE TABLE Roles(
-	usuari INT,
-    folder INT,
-    role VARCHAR(255),
+	`usuari` INT NOT NULL,
+    `folder` INT NOT NULL,
+    `role` VARCHAR(255) NOT NULL,
     FOREIGN KEY (usuari) REFERENCES Usuari(id),
     FOREIGN KEY (folder) REFERENCES Folder(id)
 );
-
