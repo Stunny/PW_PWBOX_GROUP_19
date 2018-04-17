450bf88451a6317a5d07246fcdd9b16f53329caf
Alex Vogel <avogelm@gmail.com>
2018-04-17 16:22:45 +0200

    Correos electronicos enviandose correctamente utilizando SMTP con el componente `phpmailer/phpmailer`

--- src/model/use_cases/UserUseCases/UseCaseGenerateVerificationMail.php
+++ src/model/use_cases/UserUseCases/UseCaseGenerateVerificationMail.php
@@ -9,10 +9,16 @@
 namespace PWBox\model\use_cases\UserUseCases;
 
 
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer;
 use PWBox\model\repositories\UserRepository;
 
 class UseCaseGenerateVerificationMail
 {
+
+    private const EMAIL_ADDR = "pwbox.g19@gmail.com";
+    private const EMAIL_PSSW = "kappaANGELkappa";
+
     private $repository;
 
     /**
@@ -26,21 +32,49 @@ class UseCaseGenerateVerificationMail
 
     public function __invoke($verificationHash, $userEmail)
     {
-        $headers = 'From:noreply@pwbox.test' . "\r\n";
-        $to = $userEmail;
-        $subject = '[PWBOX] Signup | Verification';
+
         $msg = '
         
-Thanks for signing up on PWBOX!
+<h1 style="color:#42a5f5">Thanks for signing up on PWBOX!</h1>
 
 Your account has been created successfully, but in order for you to enjoy the full experience of our service we need you to activate your account by confirming us your email address using the following URL:
+<a href="http://pwbox.test/api/user/verify/'.$verificationHash.'">Verify Account Email</a>
+
+If the link doesn\'t work, try pasting the following address on your browser: 
 http://pwbox.test/api/user/verify/'.$verificationHash.'
 
 We hope our service is to your liking. If you have any trouble when using it be sure to check out the documentation we provide in the following site:
 http://pwbox.test/documentation
 
         ';
-        echo $msg;
-        echo mail($to, $subject, $msg, $headers);
+
+        try{
+            $mail = new PHPMailer();
+            $mail->setFrom('noreply@pwbox.test', 'PWBox Team');
+            $mail->addAddress($userEmail);
+            $mail->Subject = '[PWBox] Sign up | Verification';
+            $mail->Body = $msg;
+            $mail->isHTML(true);
+
+            $mail->IsSMTP();
+            $mail->SMTPDebug = 2;
+            $mail->SMTPAuth = true;
+            $mail->SMTPSecure = 'tls';
+            $mail->Host = "smtp.gmail.com";
+            $mail->Mailer = "smtp";
+            $mail->Port = 587;
+            $mail->Username = self::EMAIL_ADDR;
+            $mail->Password = self::EMAIL_PSSW;
+
+            if(!$mail->send()) {
+                echo 'Message was not sent.';
+                echo 'Mailer error: ' . $mail->ErrorInfo;
+            } else {
+                echo 'Message has been sent.';
+            }
+        } catch (Exception $e) {
+            echo $e;
+        }
+
     }
 }
