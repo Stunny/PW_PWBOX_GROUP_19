6859a939502f001b1ebb8370893707781e4f0038
sartox <ls30927@salleurl.edu>
2018-06-27 16:06:51 +0200

    Eliminacion de imagen de perfil y carpetas del usuario al borrar su cuenta.

--- src/model/use_cases/UserUseCases/UseCaseDeleteUser.php
+++ src/model/use_cases/UserUseCases/UseCaseDeleteUser.php
@@ -10,6 +10,8 @@ namespace PWBox\model\use_cases\UserUseCases;
 
 use PWBox\model\User;
 use PWBox\model\repositories\UserRepository;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
 
 class UseCaseDeleteUser
 {
@@ -29,6 +31,20 @@ class UseCaseDeleteUser
 
         if($user != null){
             $this->repository->delete($user);
+            unlink("/home/vagrant/pwbox/public/profile_imgs/" . $user['username'] . ".jpg");
+            $dir = "/home/vagrant/pwbox/appdata/user_folders/" . $user['username'];
+
+            $it = new RecursiveDirectoryIterator($dir, RecursiveDirectoryIterator::SKIP_DOTS);
+            $files = new RecursiveIteratorIterator($it,
+                RecursiveIteratorIterator::CHILD_FIRST);
+            foreach($files as $file) {
+                if ($file->isDir()){
+                    rmdir($file->getRealPath());
+                } else {
+                    unlink($file->getRealPath());
+                }
+            }
+            rmdir($dir);
             return true;
         }else{
             return false;
